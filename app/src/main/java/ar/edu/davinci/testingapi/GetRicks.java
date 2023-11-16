dc7793fd5a4b067f4c46b2e9d1dfdc3e50c28cd8
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-11-15 22:18:15 -0300

    feat: add api response

--- app/src/main/java/ar/edu/davinci/testingapi/GetRicks.java
+++ app/src/main/java/ar/edu/davinci/testingapi/GetRicks.java
@@ -3,11 +3,35 @@ package ar.edu.davinci.testingapi;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class GetRicks extends AsyncTask<String, Integer, String> {
+    OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
     @Override
     protected String doInBackground(String... strings) {
         String url = strings[0];
-        return url;
+        String response = "";
+        try {
+            response = run(url);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return response;
     }
 
     @Override
