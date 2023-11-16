ec04af9570eecb013bb861eecc1a7f89e20f0a4b
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-11-15 21:41:58 -0300

    feat: add no connection check method

--- app/src/main/java/ar/edu/davinci/testingapi/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/testingapi/MainActivity.java
@@ -2,13 +2,32 @@ package ar.edu.davinci.testingapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void checkConnection(){
+        LinearLayout noInternetMessage = findViewById(R.id.noInternetMessage);
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if(networkInfo != null && networkInfo.isConnected()) {
+            //Async Task
+            noInternetMessage.setVisibility(View.INVISIBLE);
+        } else {
+            noInternetMessage.setVisibility(View.VISIBLE);
+            //Mensaje sin internet
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkConnection();
     }
 }
