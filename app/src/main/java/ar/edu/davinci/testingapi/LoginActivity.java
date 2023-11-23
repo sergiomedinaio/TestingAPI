f37c7e955c65c006654510dd86f167f635046d8a
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-11-22 22:11:07 -0300

    feat: get input text on login

--- app/src/main/java/ar/edu/davinci/testingapi/LoginActivity.java
+++ app/src/main/java/ar/edu/davinci/testingapi/LoginActivity.java
@@ -3,8 +3,26 @@ package ar.edu.davinci.testingapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public void login(String email, String password) {
+        Log.i("firebase", "email: " + email);
+        Log.i("firebase", "password: " + password);
+    }
+
+    public void onLoginButtonClick(View view) {
+        EditText emailInput = findViewById(R.id.email);
+        EditText passInput = findViewById(R.id.password);
+
+        String email = emailInput.getText().toString();
+        String password = passInput.getText().toString();
+
+        this.login(email, password);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
