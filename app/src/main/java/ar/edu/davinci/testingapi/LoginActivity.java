505f01be3593a8736247ebc6e20939eafccaa24b
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-11-22 22:24:54 -0300

    feat: login with email and password

--- app/src/main/java/ar/edu/davinci/testingapi/LoginActivity.java
+++ app/src/main/java/ar/edu/davinci/testingapi/LoginActivity.java
@@ -1,17 +1,40 @@
 package ar.edu.davinci.testingapi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     public void login(String email, String password) {
         Log.i("firebase", "email: " + email);
         Log.i("firebase", "password: " + password);
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Falló el login.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     public void onLoginButtonClick(View view) {
@@ -28,5 +51,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
     }
 }
